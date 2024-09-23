8a6161f4835ad8e47366eeb49ca28fade3fce17b
Kryusin <2210441@ecc.ac.jp>
2024-09-24 00:35:37 +0900

    add: tailwind config type js

---
+++ tailwind/tailwind.config.js
@@ -0,0 +1,47 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          light: "#4da6ff",
+          DEFAULT: "#0066cc",
+          dark: "#004080",
+        },
+        secondary: {
+          light: "#ffd699",
+          DEFAULT: "#ffaa00",
+          dark: "#cc8800",
+        },
+      },
+      fontFamily: {
+        sans: [
+          "Inter",
+          "ui-sans-serif",
+          "system-ui",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          "Segoe UI",
+          "Roboto",
+          "Helvetica Neue",
+          "Arial",
+          "Noto Sans",
+          "sans-serif",
+        ],
+      },
+      spacing: {
+        128: "32rem",
+        144: "36rem",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
+};
